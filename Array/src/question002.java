23b32a01d999f840938483c8fafa86802410c040
Omkar Pisal <omkarpisal369@gmail.com>
2025-12-22 10:11:05 -0800

    add whole array

---
+++ Array/src/question002.java
@@ -0,0 +1,11 @@
+public class question002 {
+    public static void main(String[] args) {
+        // add whole array
+        int arr [] = {1,2,3,4,5};
+        int sum = 0;
+        for(int i=0 ; i< arr.length;i++){
+           sum = sum +arr[i];
+        }
+        System.out.println(sum);
+    }
+}

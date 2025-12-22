5bb7a016b25e12a016756cd1c4d66e5d986e4773
Omkar Pisal <omkarpisal369@gmail.com>
2025-12-22 10:04:29 -0800

    lopp through array

---
+++ Array/src/question001.java
@@ -0,0 +1,9 @@
+public class question001 {
+    public static void main(String[] args) {
+        // loop through array
+        int arr [] ={10,20,30,40};
+        for(int i = 0 ; i< arr.length ; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}

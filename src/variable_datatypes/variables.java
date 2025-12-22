7e06487a47c1ba49b742194c5e2950d253b4a836
Omkar Pisal <omkarpisal369@gmail.com>
2025-12-22 02:46:29 -0800

    variables

---
+++ src/variable_datatypes/variables.java
@@ -0,0 +1,9 @@
+package variable_datatypes;
+
+public class variables {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        System.out.println(a+b);
+    }
+}

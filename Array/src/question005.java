4604cda8092d13fc9ec56b2cdfadb243503b49dd
Omkar Pisal <omkarpisal369@gmail.com>
2025-12-23 05:37:06 -0800

    print even nums

--- Array/src/question005.java
+++ Array/src/question005.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class question005 {
+public class question005 {
+    public static void main(String[] args) {
+        // even number
+        int arr [] = {1,2,3,4,5,6,7,8,9,0};
+        int count = 0;
+        for(int i = 0 ; i< arr.length;i++){
+            if(arr[i]%2 == 0){
+                System.out.println(arr[i]);
+                count++;
+            }
+        }
+        System.out.println("total even number are" +count);
+    }
 }

7546f1d87b15f140c58c7c760f404f07c0f9c22e
Omkar Pisal <omkarpisal369@gmail.com>
2025-12-23 05:43:57 -0800

    print odd nums

--- Array/src/question006.java
+++ Array/src/question006.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class question006 {
+public class question006 {
+    public static void main(String[] args) {
+        // odd nums
+        int arr [] = {1,2,3,4,5,6,7,8,9,0};
+        int count = 0;
+        for(int i = 0 ; i< arr.length;i++){
+            if(arr[i]%2 != 0){
+                System.out.println(arr[i]);
+                count++;
+            }
+        }
+        System.out.println("total even number are" +count);
+    }
 }
+

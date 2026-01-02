d37d35a5b403f05e536619fce0a542d1c0b5c9e1
Omkar Pisal <omkarpisal369@gmail.com>
2026-01-02 04:25:01 -0800

    min element in array

--- Array/src/question004.java
+++ Array/src/question004.java
@@ -1,6 +1,6 @@
 public class question004 {
     public static void main(String[] args) {
-        //min element in array
+        //min element in array ..
         int arr [] = {5,74,9,0};
         int min = arr[0];
         for(int i = 1 ; i< arr.length;i++){

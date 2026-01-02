4b18aaf020c49db207500ea143a29208102d36c0
Omkar Pisal <omkarpisal369@gmail.com>
2026-01-02 04:08:20 -0800

    Compiletime Polymorphism

--- opps/src/opps003.java
+++ opps/src/opps003.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class opps003 {
+//compile time polymorphism
+class Add{
+
+    int sum(int a , int b){
+        return a+b;
+    }
+
+    int sum(int a , int b , int c){
+        return a+b+c;
+    }
+
+
+}
+public class opps003 {
+    public static void main(String[] args) {
+        Add a1 = new Add();
+        System.out.println(a1.sum(5,5));
+        System.out.println(a1.sum(5,5,5));
+
+
+    }
 }

7fa76e2c4920ae7d2e294f3623bcc78267c5deb1
Omkar <omkarpisal369@gmail.com>
2026-03-15 17:50:09 +0530

    reverseString

--- strings/src/question001.java
+++ strings/src/question001.java
@@ -1,22 +1,23 @@
 package strings.src;
 
 import java.util.Arrays;
+    public class question001 {
+        public static void reverseString (char[] s){
+            int left = 0;
+            int right = s.length-1;
 
-public class question001 {
-    public static void reverseString (char[] s){
-        int left = 0;
-        int right = s.length-1;
+            while (left<right){
+                char temp = s[left];
+                s[left++] = s[right];
+                s[right--] = temp;
+            }
+        }
+        public static void main(String[] args) {
+            char[] s = {'h','e','l','l','o'};
+            reverseString(s);
+            System.out.println("The reverse string is : "+ Arrays.toString(s));
 
-        while (left<right){
-            char temp = s[left];
-            s[left++] = s[right];
-            s[right--] = temp;
         }
     }
-    public static void main(String[] args) {
-        char[] s = {'h','e','l','l','o'};
-       reverseString(s);
-        System.out.println("The reverse string is : "+ Arrays.toString(s));
 
-    }
-}
+

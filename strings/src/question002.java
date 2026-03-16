fc3cd0e80cf52b0bb4f4413b1a11c828d1ff72b5
Omkar <omkarpisal369@gmail.com>
2026-03-16 11:56:48 +0530

    Valid Palindrome

---
+++ strings/src/question002.java
@@ -0,0 +1,37 @@
+package strings.src;
+
+public class question002 {
+
+    public static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right){
+
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+
+            } else if (!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+
+            } else if (Character.toLowerCase(s.charAt(left)) !=
+                    Character.toLowerCase(s.charAt(right))){
+                return false;
+
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "A man, a plan, a canal: Panama";
+
+        boolean result = isPalindrome(s);
+
+        System.out.println(result);
+    }
+}

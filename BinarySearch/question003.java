ee1c92d0066614573733ddd75d564d85b2a3592c
Omkar <omkarpisal369@gmail.com>
2026-06-11 16:25:16 +0530

    search in rotated array

---
+++ BinarySearch/question003.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+//search in rotated array
+public class question003 {
+    static int rotated(int nums[] , int target){
+        int left = 0 ; int right = nums.length-1;
+        while (left<= right){
+            int mid = left + (right-left)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            if(nums[left] <= nums[mid]){
+                if(nums[left]<=target && target<= nums[mid] ){
+                    right = mid-1; // target left meh
+                } else {
+                    left = mid+1; //target right meh
+                }
+            }
+            else {
+                if(nums[mid]<=target && target<=nums[right]){
+                    left = mid+1;
+                } else {
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int nums [] = {4, 5, 6, 7, 0, 1, 2};
+        int target = 7 ;
+        System.out.println(rotated(nums , target));
+    }
+}

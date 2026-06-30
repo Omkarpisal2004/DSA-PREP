b3544bbfc6399ba45d923af601f6e9026e30bf6a
Omkar <omkarpisal369@gmail.com>
2026-06-30 13:05:30 +0530

    online stock span

--- Stack/question002.java
+++ Stack/question002.java
@@ -18,7 +18,7 @@ public class question002 {
         return ans;
     }
     public static void main(String[] args) {
-int arr[] = {1,3,2,4};
+int arr[] = {2,1,5,3};
         System.out.println(Arrays.toString(NGR(arr)));
     }
 }

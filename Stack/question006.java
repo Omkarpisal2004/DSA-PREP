534e162d6f44c079b77d33391e94a4008bd376f2
Omkar <omkarpisal369@gmail.com>
2026-06-30 13:08:01 +0530

    online stock span

--- Stack/question006.java
+++ Stack/question006.java
@@ -1,5 +1,5 @@
 package Stack;
-// online stock span
+// online stack span
 import java.util.Arrays;
 import java.util.Stack;
 
@@ -22,7 +22,8 @@ public class question006 {
         return span;
     }
     public static void main(String[] args) {
-int arr[] = {100, 80, 60, 70, 60, 75, 85};
+        int arr[] = {100, 80, 60, 70, 60, 75, 85};
         System.out.println(Arrays.toString(stockSpan(arr)));
     }
 }
+

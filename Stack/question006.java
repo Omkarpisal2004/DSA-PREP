25fb7c48fa0595d27527c387f1b7fa88ab24fd1d
Omkar <omkarpisal369@gmail.com>
2026-06-30 13:05:54 +0530

    online stock span

---
+++ Stack/question006.java
@@ -0,0 +1,28 @@
+package Stack;
+// online stock span
+import java.util.Arrays;
+import java.util.Stack;
+
+public class question006 {
+    static int[] stockSpan(int []arr){
+        Stack<Integer> st = new Stack<>();
+        int span[] = new int[arr.length];
+        for (int i=0 ; i< arr.length;i++){
+            while (!st.isEmpty() &&arr[ st.peek()] <= arr[i]){
+                st.pop();
+            }
+
+            if (st.isEmpty()){
+                span[i] = i+1;
+            } else {
+                span[i] = i - st.peek();
+            }
+            st.push(i);
+        }
+        return span;
+    }
+    public static void main(String[] args) {
+int arr[] = {100, 80, 60, 70, 60, 75, 85};
+        System.out.println(Arrays.toString(stockSpan(arr)));
+    }
+}

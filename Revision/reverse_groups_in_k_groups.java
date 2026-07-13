a65bff993f2e79c2adaef6a4a5c8127f1a8e2d85
Omkar <omkarpisal369@gmail.com>
2026-07-13 20:15:07 +0530

    reverse nodes in k group

---
+++ Revision/reverse_groups_in_k_groups.java
@@ -0,0 +1,70 @@
+package Revision;
+
+import LinkedList.question012;
+
+public class reverse_groups_in_k_groups {
+    static class  ListNode{
+        int val ;
+        ListNode next ;
+
+        ListNode(int val){
+            this.val = val;
+        }
+    }
+    static ListNode reverseKGroups(ListNode head , int k){
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prevGroup = dummy;
+
+        while (true){
+
+           ListNode kth = prevGroup;
+            // first kth group
+            for (int i = 0 ; i<k && kth != null ; i++){
+                kth = kth.next;
+
+            }
+            if(kth == null){
+                break;
+            }
+
+           ListNode nextGroup = kth.next;
+
+            ListNode curr = prevGroup.next;
+            ListNode prev = nextGroup;
+
+            while (curr != nextGroup){
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            //reconnect
+            ListNode temp = prevGroup.next;
+
+            prevGroup.next = kth;
+            prevGroup = temp;
+        }
+        return dummy.next;
+
+    }
+    static ListNode create(int arr[]){
+        ListNode head = new ListNode(arr[0]);
+        ListNode curr = head;
+        for (int i = 1 ; i< arr.length ; i++){
+            curr.next = new ListNode(arr[i]);
+            curr = curr.next;
+        }
+        return head;
+    }
+    public static void main(String[] args) {
+        ListNode head = create(new int[]{1,2,3,4,5});
+        int k = 3 ;
+        ListNode ans = reverseKGroups(head , k);
+        while (ans != null){
+            System.out.print(ans.val + "->");
+            ans =ans.next;
+        }
+    }
+}
